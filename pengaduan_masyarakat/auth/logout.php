6ce0ba7cfd06e48be363c3c9108b09361e00bfd3
Milan parade s. <119832452+KaedeharaMilan@users.noreply.github.com>
2024-12-12 21:50:20 +0700

    Delete pengaduan_masyarakat directory

--- pengaduan_masyarakat/auth/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); // -> Harus ditambahkan ketika menggunakan session
-
-session_unset();
-
-session_destroy();
-
-header('location: login.php');
-exit;
--- pengaduan_masyarakat/connect.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "pengaduan_masyarakat"); // Membuat koneksi ke mysql
-?> 
